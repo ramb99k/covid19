07a3110729b21581d15e2441522a125359f5e5f5
ramb99k <62419110+ramb99k@users.noreply.github.com>
2020-03-20 21:20:01 +0900

    all change 1

--- pages/cards/_card.vue
+++ pages/cards/_card.vue
@@ -136,7 +136,7 @@ export default {
         ? `${url}/ogp/${this.$route.params.card}.png?t=${timestamp}`
         : `${url}/ogp/${this.$i18n.locale}/${this.$route.params.card}.png?t=${timestamp}`
     const description = `${this.updatedAt} | ${this.$t(
-      '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、東京都が開設したものです。'
+      '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、海山県が開設したものです。'
     )}`
 
     return {
@@ -153,7 +153,7 @@ export default {
           content:
             this.title +
             ' | ' +
-            this.$t('東京都') +
+            this.$t('海山県') +
             ' ' +
             this.$t('新型コロナウイルス感染症') +
             this.$t('対策サイト')
