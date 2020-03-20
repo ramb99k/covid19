f9a282160cab4dc544ef0a5bb6fceddfcbf246b6
ramb99k <62419110+ramb99k@users.noreply.github.com>
2020-03-20 18:55:03 +0900

    Update print.vue

--- layouts/print.vue
+++ layouts/print.vue
@@ -2,7 +2,7 @@
   <v-app class="app-print">
     <div v-if="loading" class="loader">
       <img src="/logo.svg" :alt="$t('東京都')" />
-      <scale-loader color="#00A040" />
+      <scale-loader color="#AAAAAA" />
     </div>
     <v-container v-else>
       <v-row align="center" class="PrintMeta">
